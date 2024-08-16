5f56b80635fc96f8a4c9974e95bb985c41fb9044
dev008 <joao@pontogovsistemas.com.br>
2024-08-16 14:46:43 -0300

    Alteração send.py

--- rabbitMQ/send.py
+++ rabbitMQ/send.py
@@ -8,4 +8,6 @@ channel.queue_declare(queue='hello')
 
 channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
 print(" [x] Sent 'Hello World!'")
-connection.close()  
+connection.close()      
+
+## alteração teste
